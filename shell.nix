321eb7621f0dabe9df37b2e7f2efd8264d2468d1
Asmund Berge <asmund@berge.id>
2024-12-01 20:37:36 +0100

    Project setup

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {}
+}:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    openscad
+    openscad-lsp
+    freecad
+    orca-slicer
+  ];
+
+  shellHook = ''
+    LOCALE_ARCHIVE="$(nix-build --no-out-link '<nixpkgs>' -A glibcLocales)/lib/locale/locale-archive"
+
+    export LANG=en_GB.UTF-8
+  '';
+}
